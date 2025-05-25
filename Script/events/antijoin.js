af8a329bf0116cd1afc022d8a8b0c3cfdf6349e2
storely35221 <dabisama111@gmail.com>
2025-05-25 11:47:34 +0100

    Update antijoin.js

--- Script/events/antijoin.js
+++ Script/events/antijoin.js
@@ -1,25 +1,37 @@
 module.exports.config = {
- name: "antijoin",
- eventType: ["log:subscribe"],
- version: "1.0.0",
- credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
- description: "Welcome new members to the group"
+  name: "antijoin",
+  eventType: ["log:subscribe"],
+  version: "1.0.0",
+  credits: "يونو",
+  description: "منع دخول الأعضاء الجدد مع رسائل ظريفة"
 };
 
 module.exports.run = async function ({ event, api, Threads, Users }) {
- 	let data = (await Threads.getData(event.threadID)).data
- 	if (data.newMember == false) return;
- 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return
-    else if(data.newMember == true) {
-    var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
-			for (let idUser of memJoin) {
-					await new Promise(resolve => setTimeout(resolve, 1000));
-					api.removeUserFromGroup(idUser, event.threadID, async function (err) {
-                        if (err) return data["newMember"] = false;
-                            await Threads.setData(event.threadID, { data });
-                              global.data.threadData.set(event.threadID, data);
-                    })
-			}
- 	return api.sendMessage(`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`, event.threadID);
- }
+  let data = (await Threads.getData(event.threadID)).data;
+  if (data.newMember == false) return;
+
+  // تجاهل إذا البوت هو اللي انضاف
+  if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return;
+
+  // تنفيذ الطرد إذا كانت الخاصية مفعّلة
+  if (data.newMember == true) {
+    let memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId);
+
+    for (let idUser of memJoin) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      api.removeUserFromGroup(idUser, event.threadID, async function (err) {
+        if (err) {
+          data["newMember"] = false;
+        } else {
+          await Threads.setData(event.threadID, { data });
+          global.data.threadData.set(event.threadID, data);
+        }
+      });
+    }
+
+    return api.sendMessage(
+      `✨خاصية "منع الانضمام" مفعّلة حاليًا يا جميل💅🏼\n\nلا يمكن إضافة أعضاء جدد حتى يتم إيقاف الخاصية ❌\n\nخلي الفريق بنكهته الحلوة كذا بس، لا تخلطونا بأحد جديد! 🧁💖`,
+      event.threadID
+    );
+  }
 }
